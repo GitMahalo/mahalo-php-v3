af2409dc49618e371432d8ea48e6d82cfe7383fc
Adeline Belin <abelin@tbsblue.com>
2020-11-18 17:17:01 +0100

    Mise en forme des commentaires

--- cartebancaire/creationCarteBancaire.php
+++ cartebancaire/creationCarteBancaire.php
@@ -3,13 +3,13 @@
 	
 	// CREATION D'UNE CARTE BANCAIRE	
 	$cb = [];		
-	$cb["token"] = 'SMLLwsqPLdt'; // token
+	$cb["token"] = 'SMLLwsqPLdt'; // token (obligatoire)
 	// $cb["cbCode"] = null; // pour une creation cb 
-	$cb["dateVal"] = '2109'; // date d'expiration de la cb au format 'yyMM'
+	$cb["dateVal"] = '2109'; // date d'expiration de la cb au format 'yyMM' (obligatoire pour les traitements de relance de CB expirÃ©s)
 	$cb["firstNumbers"] = 1234; // premiers chiffres d'une cb
 	$cb["lastNumbers"] = 9876; // derniers chiffres d'une cb
 	$cb["titulaire"] = 'NOM PRENOM'; // nom prenom du titulaire de la cb
-	$cb["refPrestataire"] = 1; // référence du prestataire de paiement
+	$cb["refPrestataire"] = 1; // reference du prestataire de paiement
 
 	
 	//TRAITEMENT DES CALL API
@@ -17,13 +17,13 @@
 	$token = getToken(LOGIN,CREDENTIAL);
 	
 	print "Creation d'une carte bancaire<br><br>";
-	print "Données en entrée de la nouvelle carte bancaire : <br>";
+	print "Donnees en entree de la nouvelle carte bancaire : <br>";
 	print "Token = ".$cb["token"]."<br>";
-	// print "cbCode vaut null pour la création d'une nouvelle carte <br>";
-	print "Date d'expiration de la cb à mettre au format 'yyMM' = ".$cb["dateVal"]."<br>";
-	print "Premiers chiffres d'une cb (pas obligatoire) : ".$cb["firstNumbers"]."<br>";
-	print "Derniers chiffres d'une cb (pas obligatoire) : ".$cb["lastNumbers"]."<br>";
-	print "Référence du prestataire de paiement = ".$cb["refPrestataire"]."<br><br>";
+	// print "cbCode vaut null pour la creation d'une nouvelle carte <br>";
+	print "Date d'expiration de la cb a mettre au format 'yyMM'= ".$cb["dateVal"]."<br>";
+	print "Premiers chiffres d'une cb : ".$cb["firstNumbers"]."<br>";
+	print "Derniers chiffres d'une cb : ".$cb["lastNumbers"]."<br>";
+	print "Reference du prestataire de paiement  = ".$cb["refPrestataire"]."<br><br>";
 	
 	$response = callApiPost("/editeur/".REF_EDITEUR."/cartebancaire", $token, $cb);
 	
