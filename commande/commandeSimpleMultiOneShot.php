1642ec9d1f6bd1e0b9b675c9c1862b16c0b6a925
Adeline Belin <abelin@tbsblue.com>
2020-11-25 15:37:20 +0100

    Correction coquille pour l'exemple WS commande

--- commande/commandeSimpleMultiOneShot.php
+++ commande/commandeSimpleMultiOneShot.php
@@ -5,7 +5,7 @@
 
 	// Creation du tampon client
 	$commandeDuclient = [];
-	$commandeDuclient["codeClient"] = 0000; //codeClient retourne par l'api get client pour affecter la commande au client existant*
+	$commandeDuclient["codeClient"] = 2490964; //codeClient retourne par l'api get client pour affecter la commande au client existant*
 	$commandeDuclient["email"] = "email@email.fr";
 	$commandeDuclient["nePasModifierClient"] = 1; //permet de ne pas ecraser les donnees adresse client
 	$commandeDuclient["noCommandeBoutique"] = "CMDRHF"; //on peut passer ici une reference personnalisee de commande
@@ -15,25 +15,25 @@
 	//commande multilignes
 
 	//Creation d'une ligne de commande d'abonnement
-	$ligneCommande0["refTarif"] = 10; // Reference interne du tarif d'abonnement obtenu par l'api tarif
+	$ligneCommande0["refTarif"] = 39370; // Reference interne du tarif d'abonnement obtenu par l'api tarif
 	$ligneCommande0["quantite"] = 1;
 	$ligneCommande0["modePaiement"] = 2; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)
 	$ligneCommande0["typeAdresseLiv"] =0; //pour ne pas gerer d'adresse de livraison (l'adresse de livraison est geree via la nouvelle API createOrUpdateAdresse)
 	$commandeDuclient["lignesCommande"][] = $ligneCommande0;
 
 	//Creation d'une ligne de commande d'article libre
-	$ligneCommande0["refTarif"] = 11; // Reference interne du tarif d'article libre obtenu par l'api tarif
+	$ligneCommande1["refTarif"] = 33521; // Reference interne du tarif d'article libre obtenu par l'api tarif
 	$ligneCommande1["quantite"] = 1;
 	$ligneCommande1["modePaiement"] = 2; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)
-	$ligneCommande1["montantTtc"] = 12; //le montant peut ete force, attention donc e passer le montant exact, sinon ne pas passer cette rubrique
+	// $ligneCommande1["montantTtc"] = 12; //le montant peut ete force, attention donc a passer le montant exact, sinon ne pas passer cette rubrique
 	$ligneCommande1["typeAdresseLiv"] =0; //pour ne pas gerer d'adresse de livraison (l'adresse de livraison est geree via la nouvelle API createOrUpdateAdresse)
 	$commandeDuclient["lignesCommande"][] = $ligneCommande1;
 
 	//Creation d'une ligne de commande d'article libre
-	$ligneCommande0["refTarif"] = 12; // Reference interne du tarif d'article libre obtenu par l'api tarif
+	$ligneCommande2["refTarif"] = 33526; // Reference interne du tarif d'article libre obtenu par l'api tarif
 	$ligneCommande2["quantite"] = 1;
 	$ligneCommande2["modePaiement"] = 2; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)
-	$ligneCommande2["montantTtc"] = 12; //le montant peut ete force, attention donc e passer le montant exact, sinon ne pas passer cette rubrique
+	// $ligneCommande2["montantTtc"] = 12; //le montant peut ete force, attention donc a passer le montant exact, sinon ne pas passer cette rubrique
 	$ligneCommande2["typeAdresseLiv"] =0; //pour ne pas gerer d'adresse de livraison (l'adresse de livraison est geree via la nouvelle API createOrUpdateAdresse)
 	$commandeDuclient["lignesCommande"][] = $ligneCommande2;
 
