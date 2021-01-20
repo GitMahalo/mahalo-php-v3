75269c99f6c6492c7593218b7fc8137d1bf88c4d
Adeline Belin <abelin@tbsblue.com>
2021-01-20 15:31:25 +0100

    Exemples maj en fonction des valeurs du paramètre : typeAdresseLiv

--- commande/commandeAboAdresseLiv.php
+++ commande/commandeAboAdresseLiv.php
@@ -3,29 +3,53 @@
 
 	//PREPARATION DE LA COMMANDE
 
+	// Exemple de commandes avec des adresses de livraison temporaire uniquement pour cette commande
+	// typeAdresseLiv : paramètre qui va gérer selon sa valeur 
+	// 0 : adresse de livraison du 'codeClient'
+	// 2 : nouvelle adresse de livraison qui crée un nouveau client
+	// 7 : adresse du beneficiaire identifie par le 'codeClientLiv'
+	// Remarque : le code client doit apartenir à un 'Tiers' dans cas de livraison de type = 2
+
 	// Creation du tampon client
 	$commandeDuclient = [];
-	$commandeDuclient["codeClient"] = 543283; //codeClient retourne par l'api client get (lecture) si absent, le client sera cree lors de la validation de la commande
-	$commandeDuclient["nePasModifierClient"] = 1; //permet de ne pas ecraser les donnees adresse client (e utiliser uniquement si le codeClient est passe en parametre)
-	$commandeDuclient["refSociete"] = REF_SOCIETE; //Obligatoire identifiant de la societe
+	// Le code client doit apartenir à un Tiers dans cas de livraison de type = 2
+	$commandeDuclient["codeClient"] = 2023952; //codeClient retourne par l'api client get (lecture) si absent, le client sera cree lors de la validation de la commande
+	$commandeDuclient["nePasModifierClient"] = 1; //permet de ne pas ecraser les donnees adresse client
+	$commandeDuclient["refSociete"] = REF_SOCIETE; //OBLIGATOIRE identifiant de la societe
 	$commandeDuclient["lignesCommande"] = array();
 
-	//Creation d'une ligne de commande d'abonnement prepaye (tarif d'abonnement gratuit dans Aboweb)
-	$ligneCommande0["refTarif"] = 12; // reference unique de l'abonnement gratuit correspondant e 5euro de carte cadeau obtenu par l'api tarif
+	// Creation d'une ligne de commande d'abonnement pour une adresse temporaire
+	// Avec création d'un nouveau bénéficiaire à chaque fois
+	$ligneCommande0["refTarif"] = 1153; // reference unique de l'abonnement obtenu par l'api tarif
 	$ligneCommande0["quantite"] = 1;
-	$ligneCommande0["modePaiement"] = 2; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)//2 CB
-	$ligneCommande0["typeAdresseLiv"] = 5; //Creation d'une adresse temporaire utilise uniquement pour la commande
+	$ligneCommande0["modePaiement"] = 1; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)//2 CB
+	$ligneCommande0["typeAdresseLiv"] = 2; // pour gerer une nouvelle adresse de livraison au nouveau client bénéficiaire
+	// Adresse du nouveau client a livrer (pas possible d'utiliser le codeClientLiv dans ce cas...)
 	$ligneCommande0["civiliteLiv"] = 'M';
-	$ligneCommande0["nomLiv"] = 'LABAS';
-	$ligneCommande0["prenomLiv"] = 'LOIN';
-	$ligneCommande0["adresse2Liv"] = '22 AV DE LA GARE';
-	$ligneCommande0["cpLiv"] = '1000000';
-	$ligneCommande0["villeLiv"] = 'PEKIN';
-	$ligneCommande0["codeIsoPaysLiv"] = "CN";
+	$ligneCommande0["nomLiv"] = 'LABAS1';
+	$ligneCommande0["prenomLiv"] = 'LOIN1';
+	$ligneCommande0["adresse2Liv"] = '22 AV DE LA GARE 1';
+	$ligneCommande0["cpLiv"] = '35000';
+	$ligneCommande0["villeLiv"] = 'VILLELIV1';
+	$ligneCommande0["codeIsoPaysLiv"] = "FR";
 	$commandeDuclient["lignesCommande"][] = $ligneCommande0;
 
-	$ligneCommande = array();
-	$ligneCommande[0] = $ligneCommande0;
+	// Creation d'une ligne de commande d'abonnement pour une adresse temporaire
+	// Dans le cas où le bénéficiaire existe déjà
+	$ligneCommande1["refTarif"] = 1131; // reference unique de l'abonnement obtenu par l'api tarif
+	$ligneCommande1["quantite"] = 2; // nombre d'exemplaire
+	$ligneCommande1["modePaiement"] = 1; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)//2 CB
+	$ligneCommande1["typeAdresseLiv"] = 7; //la ligne de commande est affectee au beneficiaire identifie par le codeClientLiv
+	$ligneCommande1["codeClientLiv"] = 2023969; //codeClient du beneficiaire 
+	$commandeDuclient["lignesCommande"][] = $ligneCommande1;
+
+	// Creation d'une ligne de commande d'abonnement pour le client
+	// A livrer chez le client directement
+	$ligneCommande2["refTarif"] = 1132; // reference unique de l'abonnement obtenu par l'api tarif
+	$ligneCommande2["quantite"] = 1;
+	$ligneCommande2["modePaiement"] = 1; //1 cheque - 2 CB - 3 RIB (creation de Mandat necessaire en amont) - 4 Virement - 5 Paypal - 6 Prelevement CB (creation de CB necessaire en amont)//2 CB
+	$ligneCommande2["typeAdresseLiv"] =  0; //pour ne pas gerer d'adresse de livraison (l'adresse de livraison est geree via la nouvelle API createOrUpdateAdresse)
+	$commandeDuclient["lignesCommande"][] = $ligneCommande2;
 
 	//TRAITEMENT DES CALL API
 	$token = getToken(LOGIN,CREDENTIAL);
