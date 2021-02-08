1b7b3c068a1cc6cd927f4359ab254b31fab5c948
abelin <abelin@tbsblue.com>
2021-02-08 11:47:38 +0100

    Mise à jour de la création d'un tarif

--- tarif/creationTarif.php
+++ tarif/creationTarif.php
@@ -1,36 +1,44 @@
 <?php
 	require_once("../resttbs.php");
 	
-	// TARIF AL	
-	$tarifAl = [];	
-	$tarifAl["refSociete"] = REF_SOCIETE; // Societe sur laquelle sera créé le tarif
-	$tarifAl["codeTarif"] = 'MON_AL_11'; // Code technique du tarif
-	$tarifAl["type"] = 0; //0 AL 1 Abonnement
-	$tarifAl["codeTva"] = "A"; //Obtenu par l'api tva/list/code
-	//$tarifAl["arrondiTtc"] = 1; //Arrondir sur TVA ou non => 1 par défaut
-	$tarifAl["montantTtc"] = 10; //Obtenu
-	
-	// TARIF ABONNEMENT
-	$tarifAbo = [];
-	$tarifAbo["refSociete"] = REF_SOCIETE; // Societe sur laquelle sera créé le tarif
-	$tarifAbo["codeTarif"] = 'MON_ABO_11'; // Code technique du tarif
-	$tarifAbo["type"] = 1; //0 AL 1 Abonnement
-	$tarifAbo["refTitre"] = 1; //Obtenu par l'api titre
-	$tarifAbo["codeTva"] = "A"; //Obtenu par l'api tva/list/code
-	//$tarifAbo["arrondiTtc"] = 1; //Arrondir sur TVA ou non => 1 par défaut
-	$tarifAbo["montantTtc"] = 10; //Obtenu
-	$tarifAbo["servir"] = 1; //Nombre d'exemplaire à servir
-	
 	//TRAITEMENT DES CALL API
 	
 	$token = getToken(LOGIN,CREDENTIAL);
 	
+	// CrÃ©ation d'un Article Libre (AL)	
+	$tarifAl = [];	
+	$tarifAl["refSociete"] = REF_SOCIETE; // Societe sur laquelle sera cree le tarif
+	$tarifAl["codeTarif"] = 'MON_AL'; // Code technique du tarif - Il doit Ãªtre unique
+	$tarifAl["desiTarif"] = 'DÃ©signation de l\'AL MON_AL'; //designation de l'AL
+	$tarifAl["type"] = 0; //0 AL 1 Abonnement
+	$tarifAl["codeTva"] = "A"; //Obtenu par l'api tva/list/code
+	//$tarifAl["arrondiTtc"] = 1; //Arrondir sur TVA ou non => 1 par defaut
+	$tarifAl["montantTtc"] = 10; //Obtenu
+	$tarifAl["servir"] = 1; //  
+	$tarifAl["familleTarif"] = "REVUE"; // Famille 
+	
 	print "Creation du tarif Article Libre<br>";
 	$response = callApiPost("/editeur/".REF_EDITEUR."/tarif", $token, $tarifAl);
+	
 	if(property_exists($response, 'value')) {
 		print "refTarif du nouveau tarif = ".$response->value->refTarif."<br><br>";
 	}
 	
+	
+	
+	// CrÃ©ation d'un ABONNEMENT
+	$tarifAbo = [];
+	$tarifAbo["refSociete"] = REF_SOCIETE; // Societe sur laquelle sera cree le tarif
+	$tarifAbo["codeTarif"] = 'MON_ABO'; // Code technique du tarif - Il doit Ãªtre unique
+	$tarifAbo["desiTarif"] = 'DÃ©signation de l\'abonnement MON_ABO'; //designation de l'abonnement
+	$tarifAbo["type"] = 1; //0 AL 1 Abonnement
+	$tarifAbo["refTitre"] = 1; //Obtenu par l'api titre
+	$tarifAbo["codeTva"] = "A"; //Obtenu par l'api tva/list/code
+	//$tarifAbo["arrondiTtc"] = 1; //Arrondir sur TVA ou non => 1 par defaut
+	$tarifAbo["montantTtc"] = 10; //Obtenu
+	$tarifAbo["servir"] = 2; //Nombre d'exemplaire a servir
+	$tarifAbo["familleTarif"] = "PRELEVEMENT"; // Famille 
+
 	print "Creation du tarif Abonnement<br>";
 	$response = callApiPost("/editeur/".REF_EDITEUR."/tarif", $token, $tarifAbo);
 	
