2e66e205aa06c9ec50568ef3ccbdd5bee8bfb294
TBSDOM\fmaillard <fmaillard@tbsblue.com>
2021-04-01 15:20:17 +0200

    Modification doc optin

---
+++ client/setOptinClient.php
@@ -0,0 +1,18 @@
+<?php
+	require_once("../resttbs.php");
+	
+	// MISE A JOUR DE L'OPTIN
+	$codeClient = 11000;
+	
+	$optinClient = [];
+	$optinClient["refOptin"] = 5; //Obtenu par lecture du reférentiel d'optin : /editeur/rgpd
+	$optinClient["value"] = true; //true ou false, valeur de l'optin
+	
+	//TRAITEMENT DES CALL API
+	
+	$token = getToken(LOGIN,CREDENTIAL);
+	
+	print "Mise a jour d'un optin pour le client codeClient = ".$codeClient."<br>";
+	$response = callApiPost("/editeur/".REF_EDITEUR."/client/".$codeClient."/rgpd", $token, $optinClient);
+
+?>
---
+++ rgpd/setOptinClient.php
@@ -0,0 +1,4 @@
+<?php
+	// Les optins sont définis pour chaque clients
+	// Voir le fichier d'exemple client/setOptinClient.php
+?>
