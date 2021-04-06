cec767831eb38e991151471bb681ba08ebfa006e
abelin <abelin@tbsblue.com>
2021-04-06 12:27:00 +0200

    Mise à jour de l'exemple pour recuperer la liste liée à une origine

--- refOrigine/listeOriginesEnFonctionRefCs.php
+++ refOrigine/listeOriginesEnFonctionRefCs.php
@@ -1,16 +1,34 @@
 <?php
 	require_once("../resttbs.php");
 	
-	// Recupere la liste liées à une origine
+	// Recupere la liste liée à une origine
 
 	$refCs1 = 500; // 500 pour l'origine abonnement / 506 pour l'origine de reabonnement
 	$refSociete = 1;
-	$keyword = "";
 	
 	$params = [
 			"cs1ID" => $refCs1, // OBLIGATOIRE 500 pour l'origine abonnement / 506 pour l'origine de reabonnement
 			"refSociete" => $refSociete, // OBLIGATOIRE 
-			// "keyword" => $keyword // OPTIONNEL 
+	];
+	
+	//TRAITEMENT DES CALL API
+	
+	$token = getToken(LOGIN,CREDENTIAL);
+	
+	print "Recupere la liste liées à une origine<br><br>";
+	$response = callApiGet("/editeur/".REF_EDITEUR."/reforigine", $token, $params);
+
+
+	// Recupere la liste liée à une origine en fonction d'un mot clé
+
+	$refCs1 = 500; // 500 pour l'origine abonnement / 506 pour l'origine de reabonnement
+	$refSociete = 1;
+	$keyword = "TEST";
+	
+	$params = [
+			"cs1ID" => $refCs1, // OBLIGATOIRE 500 pour l'origine abonnement / 506 pour l'origine de reabonnement
+			"refSociete" => $refSociete, // OBLIGATOIRE 
+			"keyword" => $keyword // OPTIONNEL 
 	];
 	
 	//TRAITEMENT DES CALL API
