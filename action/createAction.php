f2e5235bbef1b04d29429be69f2a44ded1a5213d
abelin <abelin@tbsblue.com>
2021-09-02 10:34:54 +0200

    MAJ exemple création d'une action pour un client

--- action/createAction.php
+++ action/createAction.php
@@ -1,9 +1,7 @@
 <?php
 	require_once("../resttbs.php");
 	
-	$codeClient = 1000;
-	$utilisateurAbowebRappel = "UTILISATEUR_ABOWEB"; //Nom Complet de l'utilisateur Aboweb lié au rappel de l'action
-	$emailUtilisateurAbowebRappel = "utilisateurAboweb@masociete.com"; //Email de l'utilisateur Aboweb lié au rappel de l'action
+	$codeClient = 407044;
 	
 	$newAction = [];
 	$newAction["codeClient"] = $codeClient;
@@ -14,23 +12,30 @@
 	$dt = new DateTime();
 	$dt->setTimeZone(new DateTimeZone('UTC'));
 	$newAction["date"] = $dt->format('Y-m-d\TH:i:s.\0\0\0'); //date de l'action au format yyyy/mm/dd
+
 	$dt->setTimeZone(new DateTimeZone('Europe/Paris'));
 	$newAction["heure"] = $dt->format('H:i'); //heure de l'action au format hh:mm
-	$newAction["refEtat"] = 1; //Modèle Mahalo pour le template de mail obtenu par l'api etat (id) uniquement si type est Courrier ou Email
-	$newAction["etatAction"] = false; //Action deja effectuée ou non
+
+	$newAction["refEtat"] = 1; //Modele Mahalo pour le template de mail obtenu par l'api etat (id) uniquement si type est Courrier ou Email
+	$newAction["etatAction"] = false; //Action deja effectuee ou non
 	
 	$newAction["rappel"] = true; // Avec ou sans rappel
-	$rappel = new DateTime("2021-12-12 15:40",new DateTimeZone('Europe/Paris')); //Date du rappel souhaité
+	$rappel = new DateTime("2021-12-12 15:40",new DateTimeZone('Europe/Paris')); //Date du rappel souhaite
 	$newAction["heureRappel"] = $rappel->format('H:i'); //heure du rappel au format hh:mm
 	$rappel->setTime(0, 0);
 	$rappel->setTimezone(new DateTimeZone("UTC"));
 	$newAction["dateRappel"] = $rappel->format('Y-m-d\TH:i:s.\0\0\0'); //date du rappel en UTC
-	$newAction["utilRappel"] = $utilisateurAbowebRappel;
-	$newAction["rappelEmailAddress"] = $emailUtilisateurAbowebRappel;
+	$newAction["utilRappel"] = "UTILISATEUR_ABOWEB"; //Nom Complet de l'utilisateur Aboweb lie au rappel de l'action
+	$newAction["rappelEmailAddress"] = "utilisateurAboweb@masociete.com"; //Email de l'utilisateur Aboweb lie au rappel de l'action
 	
-	$newAction["etatRappel"] = 0;
-	$newAction["rappelEmail"] = 0;
+	$newAction["etatRappel"] = 0; // le rappel est fait ou non
+	$newAction["rappelEmail"] = 0; // rappeler par email
+
+
+	$newAction["texte"] = "Texte du mail pour la nouvelle action";
 	
+	// $newAction["mailLog"] = null; // optionnel : utilise pour la partie Webhook
+
 	//TRAITEMENT DES CALL API
 	
 	$token = getToken(LOGIN,CREDENTIAL);
