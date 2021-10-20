7ca872c8900d1e6af8a7bcf431dd688cbffe3975
abelin <abelin@tbsblue.com>
2021-10-20 10:31:19 +0200

    maj des commentaires pour la lecture des factures

--- facture/getFacture.php
+++ facture/getFacture.php
@@ -8,6 +8,7 @@
 	
 	$token = getToken(LOGIN,CREDENTIAL, false);
 	
-	// Affichage du pdf de la facture
+	// Affichage des données de la facture au format json
+	// Dans ce cas dans le Header de l'appel WS, il faut mettre : 'Accept: application/json'
 	$response = callApiGet("/editeur/".REF_EDITEUR."/facture/".$refFacture, $token);
 ?>
--- facture/getFacturePdf.php
+++ facture/getFacturePdf.php
@@ -9,5 +9,6 @@
 	$token = getToken(LOGIN,CREDENTIAL, false);
 	
 	// Affichage du pdf de la facture
+	// Dans ce cas dans le Header de l'appel WS, il faut mettre : 'Accept: application/pdf'
 	$response = callApiGetTypePdf("/editeur/".REF_EDITEUR."/facture/".$refFacture, $token);
 ?>
