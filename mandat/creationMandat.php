8438df82a5325dde20df2aabccccef7da15ece35
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2023-02-27 15:23:45 +0100

    Update creationMandat.php

--- mandat/creationMandat.php
+++ mandat/creationMandat.php
@@ -1,30 +1,7 @@
 <?php
-	require_once("../resttbs.php");
-	$refSociete = 1;
-	$codeClient = 753208;
-	
-	//EXEMPLE CREATION D'UN MANDAT
-	$mandat = [];
-	//MANDAT SLIMPAY
-	$mandat["tokenSepa"] = 'SLMP04038XXXX'; // token (obligatoire)
-	/*MANDAT PAYZEN
-		"tokenSepa" = "id" retournée par Payzen lors de la création du mandat, elle correspond à la concaténation de l'ID SITE et de la RUM. 
-		Exemple : https://payzen.io/fr-FR/webservices-payment/web-service-sepa/consulter-les-donnees-du-mandat.html
-	*/
-	//$mandat["tokenSepa"] = '12341243-39a4b788ae9a401fba047ddxxxxxxxxx'; // token (obligatoire)
-	$mandat["refSociete"] = $refSociete; // refSociete (obligatoire si plusieurs sociétés)
-	$mandat["codeClient"] = $codeClient; // codeClient (obligatoire)
 
-	
-	//TRAITEMENT DES CALL API
-	
-	$token = getToken(LOGIN,CREDENTIAL);
-	
-	print "Creation dun mandat<br><br>";
-	print "Donnees en entree du nouveau mandat : <br>";
-	print "Token = ".$mandat["tokenSepa"]."<br>";
-	print "refSociete = ".$mandat["refSociete"]."<br>";
-	print "codeClient = ".$mandat["codeClient"]."<br>";
-	
-	$response = callApiPost("/editeur/".REF_EDITEUR."/mandat", $token, $mandat);
+	//POUR CREATION D'UN MANDAT VIA SLIMPAY : https://github.com/GitMahalo/mahalo-php-v3/blob/master/mandat/creationMandatSlimpay.php
+
+	//POUR CREATION D'UN MANDAT VIA PAYZEN / LYRA : https://github.com/GitMahalo/mahalo-php-v3/blob/master/mandat/creationMandatPayzen.php
+
 ?>
