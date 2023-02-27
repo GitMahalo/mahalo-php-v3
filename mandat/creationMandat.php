7a52e99fc296c22136baa929e332f8698c8e5ec4
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2023-02-27 11:36:28 +0100

    Update creationMandat.php

--- mandat/creationMandat.php
+++ mandat/creationMandat.php
@@ -3,9 +3,15 @@
 	$refSociete = 1;
 	$codeClient = 753208;
 	
-	// CREATION D'UN MANDAT
+	//EXEMPLE CREATION D'UN MANDAT
 	$mandat = [];
+	//MANDAT SLIMPAY
 	$mandat["tokenSepa"] = 'SLMP04038XXXX'; // token (obligatoire)
+	/*MANDAT PAYZEN
+		"tokenSepa" = "id" retournée par Payzen lors de la création du mandat, elle correspond à la concaténation de l'ID SITE et de la RUM. 
+		Exemple : https://payzen.io/fr-FR/webservices-payment/web-service-sepa/consulter-les-donnees-du-mandat.html
+	*/
+	//$mandat["tokenSepa"] = '12341243-39a4b788ae9a401fba047ddxxxxxxxxx'; // token (obligatoire)
 	$mandat["refSociete"] = $refSociete; // refSociete (obligatoire si plusieurs sociétés)
 	$mandat["codeClient"] = $codeClient; // codeClient (obligatoire)
 
