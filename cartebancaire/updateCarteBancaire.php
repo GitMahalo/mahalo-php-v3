e15543e07bafd5986f556f0ebd916e2ba2797c6a
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2023-04-11 11:44:13 +0200

    Update updateCarteBancaire.php

--- cartebancaire/updateCarteBancaire.php
+++ cartebancaire/updateCarteBancaire.php
@@ -1,15 +1,17 @@
 <?php
 	require_once("../resttbs.php");
 	
-	// MAJ D'UNE CARTE BANCAIRE	
+	// MAJ D'UNE CARTE BANCAIRE
 	$cb = [];		
 	$cb["token"] = 'SMLLwsqPLdt'; // token
-	$cb["cbCode"] = 'PBX38687'; // maj de la cb 
+	// exemple de token pour une CB SLIMPAY : 'SMLLwsqPLdt'
+	// exemple de token pour une CB STRIPE (customer Stripe) : 'cus_HgjUPRlzIryloH'
+	$cb["cbCode"] = 'PBX38687'; // obligatoire pour maj de la cb 
 	$cb["dateVal"] = '2110'; // date d'expiration de la cb au format 'yyMM'
-	// $cb["firstNumbers"] = null; // premiers chiffres d'une cb
-	// $cb["lastNumbers"] = null; // derniers chiffres d'une cb
+	$cb["firstNumbers"] = 1234; // premiers chiffres d'une cb - Optionnel
+	$cb["lastNumbers"] = 9876; // derniers chiffres d'une cb - Optionnel
 	$cb["titulaire"] = 'NOM PRENOM'; // nom prenom du titulaire de la cb
-	$cb["refPrestataire"] = 1; // reference du prestataire de paiement (la valeur refPrestataire est à adapter selon l'éditeur)
+	//$cb["refPrestataire"] = 1; // reference du prestataire de paiement (la valeur refPrestataire est à adapter selon l'éditeur), optionnel si non défini, le prestataire sera déduit de la configuration de la société
 
 	
 	//TRAITEMENT DES CALL API
