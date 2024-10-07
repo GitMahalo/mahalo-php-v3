b1660b6a484428d9f11296bb969aa07eaf9cd88b
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2024-10-07 09:34:22 +0200

    Update creationMandat.php

--- mandat/creationMandat.php
+++ mandat/creationMandat.php
@@ -2,4 +2,6 @@
 	//POUR CREATION D'UN MANDAT VIA SLIMPAY : https://github.com/GitMahalo/mahalo-php-v3/blob/master/mandat/creationMandatSlimpay.php
 
 	//POUR CREATION D'UN MANDAT VIA PAYZEN / LYRA : https://github.com/GitMahalo/mahalo-php-v3/blob/master/mandat/creationMandatPayzen.php
+
+	//POUR CREATION D'UN MANDAT VIA STRIPE : https://github.com/GitMahalo/mahalo-php-v3/blob/master/mandat/creationMandatStripe.php
 ?>
