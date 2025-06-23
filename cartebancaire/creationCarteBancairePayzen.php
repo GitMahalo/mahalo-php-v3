508a99d963398b93f19e607ebbefee501ee82b81
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2025-06-23 09:05:30 +0200

    Update creationCarteBancairePayzen.php

--- cartebancaire/creationCarteBancairePayzen.php
+++ cartebancaire/creationCarteBancairePayzen.php
@@ -5,6 +5,10 @@
 	$cb = [];		
 	$cb["token"] = '0d9cbbd7ff0a123456978af6b61bb122'; // token (obligatoire)
 	$cb["dateVal"] = '2109'; // date d'expiration de la cb au format 'yyMM' (obligatoire pour les traitements de relance de CB expirés)
+	$cb["firstNumbers"] = 1234; // optionnel - premiers chiffres d'une cb
+	$cb["lastNumbers"] = 9876; // optionnel - derniers chiffres d'une cb
+	$cb["titulaire"] = 'NOM PRENOM'; // optionnel - nom prenom du titulaire de la cb
+	
 	// cela permet de forcer le prestataire de paiement (la valeur refPrestataire est à adapter selon l'éditeur)
 	// cette info n'est pas nécessaire dans la majorité des cas
 	// optionnel : si refPrestaire non défini, le prestataire sera déduit de la configuration de la société
