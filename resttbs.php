70a3da338caecb1769fb8a13b8faad079ec08ea7
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2026-03-30 11:52:59 +0200

    Update resttbs.php

--- resttbs.php
+++ resttbs.php
@@ -164,6 +164,12 @@
 	}
 
 	function getToken($username, $password, $json=true) {
+
+		return BEARERTOKEN;
+
+		
+		//DEPRECATED - see https://github.com/GitMahalo/mahalo-php-v3/wiki/R%C3%A9cup%C3%A9ration-du-Token-d'authentification
+		/*
 		global $urls;
 		if($json === true){ // on affiche que dans le cas ou on veut du json
 			print "<div><pre>Recuperation du token</pre></div>";
@@ -186,45 +192,8 @@
 		if($json === true){
 			print "<div><pre>TOKEN API : ".$response->access_token."</pre></div>";
 		}
-		return $response->access_token;
-		
-		/*$curl = curl_init();
-		
-		$params = [
-			'grant_type' => 'password',
-			'username' => $username,
-			'password' => $password,
-		];
-
-		$data_string = http_build_query($params);
-		//print_r($data_string);
-		//print_r($urls[TARGET]["PORTAL"]);
-
-		$opts = [
-			CURLOPT_URL => $urls[TARGET]["PORTAL"],
-			CURLOPT_POST => true,
-			CURLOPT_POSTFIELDS => $data_string,
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_SSL_VERIFYHOST => false,
-			CURLOPT_SSL_VERIFYPEER => false,
-			CURLOPT_HTTPHEADER => array(
-				'Authorization: Basic YWJvd2ViOg==',
-				'Content-Length: ' . strlen($data_string),
-				'Content-Type: application/x-www-form-urlencoded'
-			),
-		];
-
-		curl_setopt_array($curl, $opts);
-		// curl_setopt($curl, CURLOPT_VERBOSE, 1);
-		// curl_setopt($curl, CURLOPT_HEADER, 1);
-		
-		$response = curl_exec($curl);		
-		curl_close($curl);
-		// print_r($response);
-		$response = json_decode($response);
-		
-		print "TOKEN API : ".$response->access_token."<br><br>";
 		return $response->access_token;*/
+		
 	}
 
 
