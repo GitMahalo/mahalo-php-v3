374fe19eeb62c673b0afdf8718dc039858a3c678
GuillaumeColcanap <94900445+GuillaumeColcanap@users.noreply.github.com>
2026-03-30 11:46:39 +0200

    Update param.exemple.php

--- param.exemple.php
+++ param.exemple.php
@@ -1,14 +1,17 @@
 <?php	
 	define("TARGET","PREPROD"); // Valeur possible PROD / PREPROD
 	
-	define("REF_EDITEUR",null); // (Integer) Reference de l'éditeur Aboweb
-	define("REF_SOCIETE",null); // (Integer) Reference de la Société Aboweb (disponible via l'api GET editeur/{refEditeur}/societe)
+	define("REF_EDITEUR",null); // (Integer) Reference de l'Ã©diteur Aboweb
+	define("REF_SOCIETE",null); // (Integer) Reference de la SociÃ©tÃ© Aboweb (disponible via l'api GET editeur/{refEditeur}/societe)
 	
-	define("VALIDATION_COMMANDE",false); // Après l'insertion de la commande, il est possible de lancer la validation de la commande
+	define("VALIDATION_COMMANDE",false); // AprÃ¨s l'insertion de la commande, il est possible de lancer la validation de la commande
 	
 	//Utilisateur pour la connexion aux API
-	define("LOGIN","");
-	define("CREDENTIAL","");
+	//deprecated - see https://github.com/GitMahalo/mahalo-php-v3/wiki/R%C3%A9cup%C3%A9ration-du-Token-d'authentification
+	define("LOGIN","deprecated");
+	define("CREDENTIAL","deprecated");
+
+	define("BEARERTOKEN","");
 	
 	if(TARGET === ""){
 		echo "param.php => la constante TARGET est requise";
@@ -22,6 +25,7 @@
 		echo "param.php => la constante REF_SOCIETE est requise";
 		die();
 	}
+	/* DEPRECATED - see https://github.com/GitMahalo/mahalo-php-v3/wiki/R%C3%A9cup%C3%A9ration-du-Token-d'authentification
 	if(LOGIN === ""){
 		echo "param.php => la constante LOGIN est requise";
 		die();
@@ -29,7 +33,7 @@
 	if(CREDENTIAL === ""){
 		echo "param.php => la constante CREDENTIAL est requise";
 		die();
-	}
+	}*/
 
     define("ORIGINE","");
 ?>
